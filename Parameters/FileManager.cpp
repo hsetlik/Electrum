5b9fcfebc7beca618b0617e76995eb94a9e88cca
Hayden Setlik <haydensetlik@gmail.com>
2024-01-07 17:38:18 -0700

    Patch XML handling

--- Parameters/FileManager.cpp
+++ Parameters/FileManager.cpp
@@ -1,13 +1,14 @@
 #include "FileManager.h"
+#include "Identifiers.h"
 
-File UserFiles::getPresetsFolder()
+File UserFiles::getPatchesFolder()
 {
   File folder = File::getSpecialLocation(juce::File::userApplicationDataDirectory)
                     .getChildFile("ElectrumLibrary")
-                    .getChildFile("Presets");
+                    .getChildFile("Patches");
   if (!folder.exists() || !folder.isDirectory())
     folder.createDirectory();
-  DLog::log("Presets folder is at: " + String(folder.getFullPathName()));
+  DLog::log("Patches folder is at: " + String(folder.getFullPathName()));
   return folder;
 }
 
@@ -21,10 +22,72 @@ File UserFiles::getWaveformsFolder()
   return folder;
 }
 
-bool UserFiles::isValidPreset(const File &f)
+bool UserFiles::isValidPatchFile(const File &f)
 {
   if (f.getFileExtension() != ".eps")
     return false;
-
   return true;
+}
+
+void UserFiles::writePatch(ValueTree patchTree, const File &file)
+{
+  String data = patchTree.toXmlString();
+  if (!file.replaceWithText(data))
+  {
+    DLog::log("Failed to write patch at: " + String(file.getFullPathName()));
+  }
+}
+
+std::vector<String> UserFiles::getAvailablePatches()
+{
+  std::vector<String> out;
+  auto files = getPatchesFolder().findChildFiles(File::findFiles, true, "*.eps");
+  for (auto &f : files)
+    out.push_back(f.getFileNameWithoutExtension());
+  return out;
+}
+
+bool UserFiles::isLegalPatchName(const String &name)
+{
+  // TODO: decide what characters should be legal and check here
+  auto names = getAvailablePatches();
+  for (auto &p : names)
+  {
+    if (p == name)
+      return false;
+  }
+  return true;
+}
+
+ValueTree UserFiles::readPatchTree(const File &file)
+{
+  String xmlStr = file.loadFileAsString();
+  ValueTree tree = ValueTree::fromXml(xmlStr);
+  jassert(tree.hasType(IDs::ELECTRUM_PATCH));
+  return tree;
+}
+
+bool UserFiles::savePatch(ValueTree pTree, const String &name)
+{
+  if (!isLegalPatchName(name))
+  {
+    DLog::log("Warning! Illegal path name: " + name);
+    return false;
+  }
+  pTree.setProperty(IDs::patchName, name, nullptr);
+  auto file = getPatchesFolder().getChildFile(name + ".eps");
+  if (!file.existsAsFile())
+    file.create();
+  writePatch(pTree, file);
+  return true;
+}
+
+ValueTree UserFiles::treeForPatch(const String &name)
+{
+  auto pFile = getPatchesFolder().getChildFile(name + ".eps");
+  if (!pFile.existsAsFile())
+  {
+    DLog::log("Warning! No file found for patch " + name);
+  }
+  return readPatchTree(pFile);
 }
--- Parameters/FileManager.h
+++ Parameters/FileManager.h
@@ -3,8 +3,18 @@
 
 // handles the finding, reading, and writing of presets and waveforms
 namespace UserFiles {
-File getPresetsFolder();
+File getPatchesFolder();
 File getWaveformsFolder();
 
-bool isValidPreset(const File &f);
+bool isValidPatchFile(const File &f);
+void writePatch(ValueTree patchTree, const File &file);
+ValueTree readPatchTree(const File &file);
+bool isLegalPatchName(const String &name);
+
+// the above are mostly helpers, other code should use mostly these three:
+// returns a list of the unique name of each patch installed
+std::vector<String> getAvailablePatches();
+bool savePatch(ValueTree pTree, const String &name);
+ValueTree treeForPatch(const String &name);
+
 } // namespace UserFiles
