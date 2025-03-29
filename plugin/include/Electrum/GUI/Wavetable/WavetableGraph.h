f8c56e693bb0a6c6335be43eaef0a3f5d0c10b53
hsetlik <haydensetlik@gmail.com>
2025-03-29 09:51:21 -0600

    cleaned up 3d vector math

--- plugin/include/Electrum/GUI/Wavetable/WavetableGraph.h
+++ plugin/include/Electrum/GUI/Wavetable/WavetableGraph.h
@@ -8,60 +8,6 @@
 
 #include "juce_opengl/juce_opengl.h"
 
-// Stuff for 3D matrix math
-
-template <typename T>
-struct Mat3x3 {
-  T data[3][3];
-  Mat3x3() {
-    for (int r = 0; r < 3; r++) {
-      for (int c = 0; c < 3; c++) {
-        data[r][c] = (T)0.0f;
-      }
-    }
-  }
-
-  Mat3x3(Mat3x3<T>& other) {
-    for (int r = 0; r < 3; r++) {
-      for (int c = 0; c < 3; c++) {
-        data[r][c] = other.data[r][c];
-      }
-    }
-  }
-
-  juce::Vector3D<T> operator*(const juce::Vector3D<T>& vec) {
-    juce::Vector3D<T> out = {0.0f, 0.0f, 0.0f};
-    for (int c = 0; c < 3; c++) {
-      out.x = out.x + (vec.x * data[0][c]);
-      out.y = out.y + (vec.y * data[1][c]);
-      out.z = out.z + (vec.z * data[2][c]);
-    }
-    return out;
-  }
-
-  Mat3x3<T> operator*(const Mat3x3<T>& other) {
-    Mat3x3<T> out;
-    auto& oData = other.data;
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        for (int u = 0; u < 3; u++) {
-          out.data[i][j] += data[i][u] * oData[u][j];
-        }
-      }
-    }
-    return out;
-  }
-
-  void operator=(const Mat3x3<T>& other) {
-    for (int r = 0; r < 3; r++) {
-      for (int c = 0; c < 3; c++) {
-        data[r][c] = other.data[r][c];
-      }
-    }
-  }
-  static Mat3x3<float> getRotationMatrix(float x, float y, float z);
-};
-
 //==================================================================
 typedef juce::Vector3D<float> vec3D_f;
 
@@ -90,8 +36,6 @@ private:
   // position state stuff
   float lastDrawnPos = 0.0f;
   float currentPos = 0.0f;
-  // 3D projection stuff
-  Mat3x3<float> rotationMatrix;
   // and the sets of paths we need to draw
   std::stack<wave_path_t> wavePaths;
 
@@ -107,6 +51,5 @@ public:
   void loadWaveDataFrom(Wavetable* wt);
 
 private:
-  void setRotationMatrix();
   void updateGraphImage();
 };
--- plugin/source/WavetableGraph.cpp
+++ plugin/source/WavetableGraph.cpp
@@ -1,5 +1,69 @@
 #include "Electrum/GUI/Wavetable/WavetableGraph.h"
+#include "Electrum/GUI/LookAndFeel/Color.h"
+#include "Electrum/Identifiers.h"
+// Stuff for 3D matrix math
 
+template <typename T>
+struct Mat3x3 {
+  T data[3][3];
+  Mat3x3() {
+    for (int r = 0; r < 3; r++) {
+      for (int c = 0; c < 3; c++) {
+        data[r][c] = (T)0.0f;
+      }
+    }
+  }
+
+  Mat3x3(Mat3x3<T>& other) {
+    for (int r = 0; r < 3; r++) {
+      for (int c = 0; c < 3; c++) {
+        data[r][c] = other.data[r][c];
+      }
+    }
+  }
+
+  juce::Vector3D<T> operator*(const juce::Vector3D<T>& vec) {
+    juce::Vector3D<T> out = {0.0f, 0.0f, 0.0f};
+    for (int c = 0; c < 3; c++) {
+      out.x = out.x + (vec.x * data[0][c]);
+      out.y = out.y + (vec.y * data[1][c]);
+      out.z = out.z + (vec.z * data[2][c]);
+    }
+    return out;
+  }
+
+  Mat3x3<T> operator*(const Mat3x3<T>& other) {
+    Mat3x3<T> out;
+    auto& oData = other.data;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        for (int u = 0; u < 3; u++) {
+          out.data[i][j] += data[i][u] * oData[u][j];
+        }
+      }
+    }
+    return out;
+  }
+
+  void operator=(const Mat3x3<T>& other) {
+    for (int r = 0; r < 3; r++) {
+      for (int c = 0; c < 3; c++) {
+        data[r][c] = other.data[r][c];
+      }
+    }
+  }
+  static Mat3x3<float> getRotationMatrix(float x, float y, float z);
+};
+
+//===================================================
+// the rotation matrix that projects our 3D points
+// back onto a 2D bitmap
+static Mat3x3<float> _graphRotationMatrix() {
+  const float xAngle = juce::MathConstants<float>::pi * 1.0f;
+  const float yAngle = juce::MathConstants<float>::pi * -0.6f;
+  const float zAngle = juce::MathConstants<float>::pi * -0.35f;
+  return Mat3x3<float>::getRotationMatrix(xAngle, yAngle, zAngle);
+}
 // various 3D math helpers
 static std::vector<vec3D_f> getVerticesForWave(
     const std::vector<float>& normPoints,
@@ -12,7 +76,35 @@ static std::vector<vec3D_f> getVerticesForWave(
   return points;
 }
 
-static wave_path_t convertToPath(const std::vector<vec3D_f>& vertices) {}
+static juce::Point<float> projectToCanvas(vec3D_f point3d) {
+  static Mat3x3<float> rotation = _graphRotationMatrix();
+  constexpr float yHeight = 1.4f;
+  vec3D_f cameraPos = {-0.5f, yHeight, 0.0f};
+  // represents the display surface
+  vec3D_f e = {0.0f, 0.0f, CAMERA_DISTANCE};
+
+  // adjust by the camera position
+  auto pt = point3d - cameraPos;
+  // apply the rotation
+  pt = rotation * pt;
+  // back to 2d
+  const float fX = ((e.z / pt.z) * pt.x) + e.x;
+  const float fY = ((e.z / pt.z) * pt.y) + e.y;
+  return {fX * (float)GRAPH_W, fY * (float)GRAPH_H};
+}
+
+static wave_path_t convertToPath(const std::vector<vec3D_f>& vertices) {
+  // 1. draw the path
+  juce::Path path;
+  path.startNewSubPath(projectToCanvas(vertices[0]));
+  for (size_t i = 1; i < vertices.size(); ++i) {
+    path.lineTo(projectToCanvas(vertices[i]));
+  }
+  path.closeSubPath();
+  const float zPos = vertices[0].z;
+  const float strokeWeight = flerp(2.0f, 4.5f, 1.0f - zPos);
+  return {path, Color::mintGreenBright, zPos, strokeWeight};
+}
 
 //===================================================
 
