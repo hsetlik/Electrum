04cb707250f10e7027fea3400bd836a7e3bf6692
hsetlik <haydensetlik@gmail.com>
2025-04-15 15:09:40 -0600

    FrameWarp scheme

--- plugin/include/Electrum/GUI/WaveEditor/FFTView.h
+++ plugin/include/Electrum/GUI/WaveEditor/FFTView.h
@@ -7,35 +7,71 @@
 #define MAX_WARP_PTS 200
 #define DEFAULT_WARP_POINTS 30
 
+class WarpPoint {
+public:
+  const float normFreq;
+  const float originalMag;
+  float warpedMag;
+  WarpPoint(float nf, float mag) : normFreq(nf), originalMag(mag) {
+    warpedMag = originalMag;
+  }
+};
+
 #define MIN_FFT_ZOOM 0.3f
 #define MAX_FFT_ZOOM 7.0f
-
+class FrameWarp;
 #define FFT_GRAPH_RES AUDIBLE_BINS
 class FrameSpectrum : public WaveEditListener {
   static constexpr int minWidth = FFT_GRAPH_RES;
   bin_array_t loadedWaveBins;
+  bin_array_t warpWaveBins;
   bool binsReady = false;
   frange_t currentMagRange;
-  int numWarpPoints = 256;
+  int warpPoints = -1;
   int currentFrame = -1;
   float currentMaxMagnitude = 0.0f;
   float currentMedianMag = 0.04f;
   float currentZoom = 1.0f;
-  float yPosForFreq(const frect_t& bounds, float fNorm) const;
+  bool currentHasWarp = false;
+  float yPosAtFreq(const frect_t& bounds, float fNorm) const;
+  fpoint_t warpPointToCanvas(const frect_t& bounds, WarpPoint* p);
 
 public:
   FrameSpectrum(ValueTree& vt);
   void setZoomNorm(float v);
+  int numWarpPoints() const { return warpPoints; }
+  void setNumWarpPoints(int points);
   void frameWasFocused(int idx) override;
   void paint(juce::Graphics& g) override;
   void resized() override;
+  friend class FrameWarp;
+
+private:
+  std::unique_ptr<FrameWarp> warp;
+  // helpers for initializing/changing the warp points
+};
+//===============================================
+// this guy keeps track of all our edits to the the spectrum,
+// handles the business of calculating the Magnitude for each bin
+class FrameWarp {
+private:
+  FrameSpectrum* const parent;
+  juce::OwnedArray<WarpPoint> touchedPoints;
+  std::vector<WarpPoint*> editPoints;
+  void sortEditPoints();
+
+public:
+  FrameWarp(FrameSpectrum* p);
 };
 
+//=============================================
 class FrameSpectrumViewer : public Component {
 private:
   FrameSpectrum spec;
-  juce::Slider slider;
+  juce::Slider zoomSlider;
   BoundedAttString zoomStr;
+  juce::Slider pointSlider;
+  BoundedAttString pointStr;
   juce::Viewport vpt;
 
 public:
--- plugin/source/FFTView.cpp
+++ plugin/source/FFTView.cpp
@@ -2,6 +2,8 @@
 #include "Electrum/Audio/AudioUtil.h"
 #include "Electrum/Audio/Wavetable.h"
 #include "Electrum/GUI/LookAndFeel/Color.h"
+#include "Electrum/GUI/LookAndFeel/Fonts.h"
+#include "Electrum/GUI/WaveEditor/EditValueTree.h"
 #include "Electrum/Identifiers.h"
 #include "juce_core/juce_core.h"
 
@@ -14,7 +16,36 @@ static float getMagnitudeAtNormFreq(const bin_array_t& bins, float x) {
 }
 
 //===================================================
-FrameSpectrum::FrameSpectrum(ValueTree& vt) : WaveEditListener(vt) {
+// helpers for
+
+FrameWarp::FrameWarp(FrameSpectrum* s) : parent(s) {
+  auto child = parent->waveTree.getChild(parent->currentFrame);
+  jassert(child.isValid() && child.hasType(WaveEdit::WAVE_FRAME));
+  auto warpTree = child.getChildWithName(WaveEdit::FFT_WARP);
+  if (warpTree.isValid()) {  // we already have an active frame warp
+    for (auto it = warpTree.begin(); it != warpTree.end(); ++it) {
+      auto gainPt = *it;
+      jassert(gainPt.hasType(WaveEdit::FFT_GAIN_POINT));
+      const float freq = gainPt[WaveEdit::gainPointFrequency];
+
+      const float mag = gainPt[WaveEdit::gainPointMagnitude];
+      auto* wp = touchedPoints.add(new WarpPoint(freq, mag));
+      editPoints.push_back(wp);
+    }
+  }
+  sortEditPoints();
+}
+
+static std::function<bool(WarpPoint* a, WarpPoint* b)> wpCompare =
+    [](WarpPoint* a, WarpPoint* b) { return a->normFreq < b->normFreq; };
+
+void FrameWarp::sortEditPoints() {
+  std::sort(editPoints.begin(), editPoints.end(), wpCompare);
+}
+
+//===================================================
+FrameSpectrum::FrameSpectrum(ValueTree& vt)
+    : WaveEditListener(vt), warp(std::make_unique<FrameWarp>(this)) {
   frameWasFocused(0);
 }
 
@@ -35,7 +66,11 @@ void FrameSpectrum::frameWasFocused(int frame) {
   }
 }
 
-float FrameSpectrum::yPosForFreq(const frect_t& bounds, float fNorm) const {
+void FrameSpectrum::setNumWarpPoints(int points) {
+  juce::ignoreUnused(points);
+}
+
+float FrameSpectrum::yPosAtFreq(const frect_t& bounds, float fNorm) const {
   if (!binsReady)
     return bounds.getBottom();
   float magnitude = getMagnitudeAtNormFreq(loadedWaveBins, fNorm);
@@ -57,7 +92,7 @@ void FrameSpectrum::paint(juce::Graphics& g) {
   float y;
   for (size_t i = 1; i < FFT_GRAPH_RES; ++i) {
     const float xNorm = (float)i / (float)FFT_GRAPH_RES;
-    y = yPosForFreq(fBounds, xNorm);
+    y = yPosAtFreq(fBounds, xNorm);
     path.lineTo(x, y);
     x += dX;
   }
@@ -99,24 +134,69 @@ FrameSpectrumViewer::FrameSpectrumViewer(ValueTree& vt) : spec(vt) {
   addAndMakeVisible(vpt);
   spec.resized();
   // 2. set up the zoom slider
-  slider.setSliderStyle(juce::Slider::LinearVertical);
-  slider.setTextBoxStyle(juce::Slider::NoTextBox, true, 1, 1);
-  slider.setRange(0.0, 1.0);
-  addAndMakeVisible(slider);
-  slider.onValueChange = [this]() {
-    const float zNorm = (float)slider.getValue();
+  zoomSlider.setSliderStyle(juce::Slider::LinearVertical);
+  zoomSlider.setTextBoxStyle(juce::Slider::NoTextBox, true, 1, 1);
+  zoomSlider.setRange(0.0, 1.0);
+  addAndMakeVisible(zoomSlider);
+  zoomSlider.onValueChange = [this]() {
+    const float zNorm = (float)zoomSlider.getValue();
     spec.setZoomNorm(zNorm);
   };
+  // 3. set up the point slider
+  pointSlider.setSliderStyle(juce::Slider::LinearVertical);
+  pointSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 35, 18);
+  pointSlider.setRange((double)MIN_WARP_PTS, (double)MAX_WARP_PTS, 1.0);
+  pointSlider.setValue((double)DEFAULT_WARP_POINTS);
+  addAndMakeVisible(pointSlider);
+  if (spec.numWarpPoints() != -1) {
+    pointSlider.setValue((double)spec.numWarpPoints());
+  } else {
+    pointSlider.setVisible(false);
+    pointSlider.setEnabled(false);
+  }
+  pointSlider.onValueChange = [this]() {
+    int nPoints = (int)pointSlider.getValue();
+    spec.setNumWarpPoints(nPoints);
+  };
+  // 4. set up the label strings
+  zoomStr.aString.setText("Zoom");
+  zoomStr.aString.setJustification(juce::Justification::centred);
+  zoomStr.aString.setFont(FontData::getFontWithHeight(FontE::RobotoMI, 12.0f));
+  zoomStr.aString.setColour(UIColor::defaultText);
+
+  pointStr.aString.setText("Warp Points");
+  pointStr.aString.setJustification(juce::Justification::centred);
+  pointStr.aString.setFont(FontData::getFontWithHeight(FontE::RobotoMI, 12.0f));
+  pointStr.aString.setColour(UIColor::defaultText);
+
+  zoomSlider.setValue(0.0003);
 }
 
 void FrameSpectrumViewer::resized() {
-  auto iBounds = getLocalBounds();
-  auto sBounds = iBounds.removeFromLeft(30);
-  slider.setBounds(sBounds);
-  vpt.setBounds(iBounds);
+  if (spec.numWarpPoints() != -1) {
+    pointSlider.setVisible(true);
+    pointSlider.setEnabled(true);
+  } else {
+    pointSlider.setVisible(false);
+    pointSlider.setEnabled(false);
+  }
+  auto fBounds = getLocalBounds().toFloat();
+  auto controlWidth = std::max(fBounds.getWidth() / 10.0f, 45.0f);
+  auto zoomBounds = fBounds.removeFromLeft(controlWidth);
+  zoomStr.bounds = zoomBounds.removeFromTop(14.0f);
+  auto pointBounds = zoomBounds.removeFromBottom(zoomBounds.getHeight() / 2.0f);
+  pointStr.bounds = pointBounds.removeFromTop(14.0f);
+  zoomSlider.setBounds(zoomBounds.toNearestInt());
+  pointSlider.setBounds(pointBounds.toNearestInt());
+  vpt.setBounds(fBounds.toNearestInt());
 }
+
 void FrameSpectrumViewer::paint(juce::Graphics& g) {
   auto fBounds = getLocalBounds().toFloat();
   g.setColour(UIColor::menuBkgnd);
   g.fillRect(fBounds);
+  zoomStr.draw(g);
+  if (pointSlider.isVisible()) {
+    pointStr.draw(g);
+  }
 }
