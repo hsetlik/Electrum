00dc0a39aaff53b3d10a14d07de023e4a51e8b08
hsetlik <haydensetlik@gmail.com>
2025-05-03 09:02:09 -0600

    Basic ApplicationCommandInfo

--- plugin/include/Electrum/GUI/WaveEditor/WaveMenu.h
+++ plugin/include/Electrum/GUI/WaveEditor/WaveMenu.h
@@ -2,6 +2,7 @@
 #include "juce_gui_basics/juce_gui_basics.h"
 
 typedef juce::ApplicationCommandTarget CommandTarget;
+typedef juce::ApplicationCommandInfo CommandInfo;
 typedef juce::CommandID CommandID;
 
 namespace WaveCmd {
@@ -9,7 +10,15 @@ namespace WaveCmd {
 const CommandID appendWaveFrame = 0;
 const CommandID insertWaveFrame = 1;
 const CommandID deleteWaveFrame = 2;
-// wave selecting--------------------------
-const CommandID focusWaveFrame = 3;
+// Interpolation--------------------------
+const CommandID insertLerpFrames = 3;
+// File menu stuff-----------------------
+const CommandID newWavetable = 4;
+const CommandID openWavetable = 5;
+
+static constexpr size_t NUM_WAVE_COMMANDS = 6;
+
+// get the info for each of the commands
+CommandInfo getInfo(CommandID id);
 
 }  // namespace WaveCmd
--- plugin/source/WaveMenu.cpp
+++ plugin/source/WaveMenu.cpp
@@ -1,1 +1,56 @@
+#include "Electrum/GUI/WaveEditor/WaveMenu.h"
+
+namespace WaveCmd {
+
+static std::vector<CommandInfo> getCommandInfoList() {
+  std::vector<CommandInfo> vec = {};
+  // append wave
+  CommandInfo cAppend(appendWaveFrame);
+  cAppend.shortName = "Append frame";
+  cAppend.description =
+      "Create a new wave frame at the end of the current table";
+  cAppend.categoryName = "Wave";
+  vec.push_back(cAppend);
+  // insert frame
+  CommandInfo cFrameInsert(insertWaveFrame);
+  cFrameInsert.shortName = "Insert frame";
+  cFrameInsert.description = "Insert a new wave frame after the selected one";
+  cFrameInsert.categoryName = "Wave";
+  vec.push_back(cFrameInsert);
+  // delete frame
+  CommandInfo cDelete(deleteWaveFrame);
+  cDelete.shortName = "Delete frames";
+  cDelete.description = "Delete the selected wave frames";
+  cDelete.categoryName = "Wave";
+  vec.push_back(cDelete);
+  // lerp frames
+  CommandInfo cLerpFrames(insertLerpFrames);
+  cLerpFrames.shortName = "Add interpolated frames";
+  cLerpFrames.description =
+      "Add some number of frames to interpolate from this frame to the next";
+  cLerpFrames.categoryName = "Wave";
+  vec.push_back(cLerpFrames);
+  // File menu-------
+  // new wavetable
+  CommandInfo cNewTable(newWavetable);
+  cNewTable.shortName = "New wavetable";
+  cNewTable.description = "Create a new wavetable file";
+  cNewTable.categoryName = "File";
+  cNewTable.addDefaultKeypress('n', juce::ModifierKeys::commandModifier);
+  vec.push_back(cNewTable);
+
+  CommandInfo cOpenTable(openWavetable);
+  cOpenTable.shortName = "Open Wavetable";
+  cOpenTable.description = "Open an existing wavetable";
+  cOpenTable.categoryName = "File";
+
+  return vec;
+}
+
+CommandInfo getInfo(CommandID id) {
+  static std::vector<CommandInfo> commands = getCommandInfoList();
+  return commands[(size_t)id];
+}
+
+}  // namespace WaveCmd
 //===================================================
